590d4cf22cd118c4079a9a910078189b75debe23
Piotr Sułek <piotrsulek123@gmail.com>
2024-03-15 14:47:01 +0100

    added methods

--- src/zad1/Service.java
+++ src/zad1/Service.java
@@ -7,5 +7,25 @@
 package zad1;
 
 
+import java.util.Locale;
+
 public class Service {
-}  
+    private String countryName;
+    private Locale country = new Locale(countryName);
+
+    public Service(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeather(String town) {
+        return town;
+    }
+
+    public Double getRateFor(String currency) {
+        return null;
+    }
+
+    public Double getNBPRate() {
+        return null;
+    }
+}
