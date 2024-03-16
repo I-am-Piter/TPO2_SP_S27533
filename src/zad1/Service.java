7e08d2232a2c2a766a6f954ce9280c9c45de5b83
Piotr Sułek <piotrsulek123@gmail.com>
2024-03-16 16:31:58 +0100

    pogoda, czesc currency

--- src/zad1/Service.java
+++ src/zad1/Service.java
@@ -7,6 +7,7 @@
 package zad1;
 
 import netscape.javascript.JSObject;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -16,6 +17,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.net.http.HttpRequest;
+import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -34,8 +37,8 @@ public class Service {
     }
 
     public String getWeather(String city) {
-        String lat;
-        String lon;
+        String lat = null;
+        String lon = null;
         String apiKey = "6ae983073ffef0266227e60865d98dd7";
         String url = "http://api.openweathermap.org/geo/1.0/direct?q="+city+","+country.getCountry()+"&appid="+apiKey;
         StringBuilder info = new StringBuilder();
@@ -66,20 +69,65 @@ public class Service {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Pattern latLon = Pattern.compile("\"lat\":(\\d+.\\d{7}),\"lon\":((\\d+.\\d{7}))");
-        Matcher matcher = latLon.matcher(info);
-        System.out.println(matcher.find());
-        matcher.group();
-        lat = matcher.group(1);
-        lon = matcher.group(2);
-        JSONObject
+        JSONObject jobj = null;
+        try {
+            jobj = new JSONObject(info.toString().replaceAll("[\\[\\]]",""));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            lat = jobj.get("lat").toString();
+            lon = jobj.get("lon").toString();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
 
+        url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKey;
 
+        info.delete(0,info.length());
+        try {
+            URL url1 = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) url1.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
 
-        return city;
+            int response = conn.getResponseCode();
+
+            if(response != 200){
+                System.out.println("nie działa");
+            }else{
+                info = new StringBuilder();
+                Scanner scanner = new Scanner(url1.openStream());
+
+                while(scanner.hasNext()){
+                    info.append(scanner.nextLine());
+                }
+
+                scanner.close();
+
+                System.out.println(info);
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            JSONObject pogoda = new JSONObject(info.toString());
+            pogoda = new JSONObject(pogoda.get("weather").toString().replaceAll("[\\[\\]]",""));
+            System.out.println(pogoda.get("main"));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return info.toString();
     }
 
     public Double getRateFor(String currency) {
+        NumberFormat currencyCode = NumberFormat.getCurrencyInstance(country);
+        System.out.println(currencyCode.getCurrency());
+        String url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_9wH1PCm7aM57EAzJzMKlbQoD68UOk7fE7Ald4qnV&currencies=EUR&base_currency=PLN";
         return null;
     }
 
